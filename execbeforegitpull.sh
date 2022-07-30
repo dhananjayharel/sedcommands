8bab3bc1800ba7651f063abc46b7f02a498cdb2c
dhananjayharel <dhananjayharel@users.noreply.github.com>
2022-07-30 18:46:35 +0530

    Update execbeforegitpull.sh

--- execbeforegitpull.sh
+++ execbeforegitpull.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 echo "fs.inotify.max_user_watches=524288" >> /etc/sysctl.conf
-
-cd /home/project
-mv $PROJECTDIR old_$PROJECTDIR
-git clone $GITURL
+echo "none"
+cd /home/project/$PROJECTDIR
+echo "download tar now"
+wget -q -O - https://s3.amazonaws.com/angularnodemodules/$PROJECTDIR.tar > op.tar
+tar -xvf op.tar > /dev/null
+echo "extracted"
+rm op.tar
 
 
 
