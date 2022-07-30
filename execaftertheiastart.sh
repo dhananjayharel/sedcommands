6759d0e48ce4b0005b9d5b1907f3ffdbe0884e13
dhananjayharel <dhananjayharel@users.noreply.github.com>
2022-07-30 19:34:49 +0530

    Update execaftertheiastart.sh

--- execaftertheiastart.sh
+++ execaftertheiastart.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 echo "none"
 
+echo "none"
+cd /home/project/$PROJECTDIR
+echo "download tar now2"
+wget -q -O - https://s3.amazonaws.com/angularnodemodules/$PROJECTDIR.tar > op.tar
+tar -xvf op.tar > /dev/null
+echo "extracted"
+rm op.tar
